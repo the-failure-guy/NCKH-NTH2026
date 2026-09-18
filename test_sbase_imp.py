285ffc5d2d4463d5b8398af3c9ab087fc68cb625
the-failure-guy <minhchillinbtw@gmail.com>
2026-09-18 13:10:36 +0700

    remove unneccessary things

--- test_sbase_imp.py
+++
@@ -1,6 +0,0 @@
-import joblib, numpy as np
-art = joblib.load('./Training/Model/MedicalCondition/sbase_model.joblib')
-xgb, features = art['model'], art['features']
-imp = xgb.feature_importances_
-idx = np.argsort(imp)[-10:]
-for i in idx: print(f"{features[i]}: {imp[i]}")
